03e299675524d82d4f00c11ed902e33c5bf1834a
Gauri Patil <gauri.be0811@gmail.com>
2023-10-10 23:58:08 +0530

    Update example12.js

--- B2_LetAndConst_let/example12.js
+++ B2_LetAndConst_let/example12.js
@@ -55,3 +55,15 @@ You'll have finer control.
 
 It'll make your coding life easier.
  */
+
+let x = 1;
+
+if (x === 1) {
+  let x = 2;
+
+  console.log(x);
+  // Expected output: 2
+}
+
+console.log(x);
+// Expected output: 1
