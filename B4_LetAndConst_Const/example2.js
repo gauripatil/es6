2dfcd633aecbf03d96b1b46a1bbb09fb3e8425a9
Gauri Patil <gauri.be0811@gmail.com>
2023-10-11 23:43:00 +0530

    Completed const

---
+++ B4_LetAndConst_Const/example2.js
@@ -0,0 +1,3 @@
+const firstname = 'Tony';
+firstname = 'Anthony'; // error Uncaught SyntaxError: Identifier 'firstname' has already been declared
+console.log(firstname);
---
+++ B4_LetAndConst_Const/example3.js
@@ -0,0 +1,5 @@
+const me = { firstname: 'Tony', lastname: 'Alicea' };
+me.firstname = 'Anthony'; // only binding is immutable, not value
+console.log(me);
+
+//OUTPUT - {firstname: 'Anthony', lastname: 'Alicea'}
