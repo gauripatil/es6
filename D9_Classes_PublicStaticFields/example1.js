8f586cb5a7336a8c148fd7f726f77fc2df5a433e
Gauri Patil <gauri.be0811@gmail.com>
2023-10-28 20:06:01 +0530

    some minor change

--- D9_Classes_PublicStaticFields/example1.js
+++ D9_Classes_PublicStaticFields/example1.js
@@ -21,3 +21,12 @@ console.log(Person.goodMorning());
 console.log(Person.residence);
 console.log(me.__proto__); // no goodMorning
 console.log(me.goodMorning()); // error
+
+/**
+ * 
+Good morning.
+Earth
+{constructor: ƒ, greet: ƒ}
+Uncaught TypeError: me3.goodMorning is not a function
+   
+ */
