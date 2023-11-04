7be69b36b0bea78b451be4af0d13914fd58f7126
Gauri Patil <gauri.be0811@gmail.com>
2023-11-04 23:59:34 +0530

    Update example1.js

--- E3_ArrowFunctions_This/example1.js
+++ E3_ArrowFunctions_This/example1.js
@@ -20,3 +20,16 @@ let me = {
 
 me.greet();
 me.greet2();
+
+"use strict";
+
+const obj = {
+  i: 10,
+  b: () => console.log(this.i, this),
+  c() {
+    console.log(this.i, this);
+  },
+};
+
+obj.b(); // logs undefined, Window { /* … */ } (or the global object)
+obj.c(); // logs 10, Object { /* … */ }
